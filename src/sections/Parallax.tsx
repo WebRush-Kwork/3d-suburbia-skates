5b63baadaf08b2c2de89fe470740477a25c192dd
WebRush-Kwork <webrushgithub@gmail.com>
2025-05-27 19:23:00 +0500

    parallax update

--- src/sections/Parallax.tsx
+++ src/sections/Parallax.tsx
@@ -1,20 +1,38 @@
 import { Bounded } from '@/components/Bounded'
 import { ButtonLink } from '@/components/ButtonLink'
 import { Heading } from '@/components/Heading'
+import Image from 'next/image'
 
 const Parallax = () => {
 	return (
 		<Bounded className='bg-[var(--brand-blue)] bg-texture text-white'>
-			<Heading size='lg' as='h2'>
-				Crafted for the kickflip
-			</Heading>
-			<div className='mb-8' />
-			<div className='max-w-md leading-relaxed text-lg'>
-				Build for big tricks and hard landings, our boards are designed to
-				handle every flip, grind, and bail. Perfect balance, every time.
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-24 items-center'>
+				<div className='flex flex-col items-center md:items-start md:text-start gap-8'>
+					<Heading size='lg' as='h2'>
+						Crafted for the kickflip
+					</Heading>
+					<div className='max-w-md leading-relaxed text-lg'>
+						Build for big tricks and hard landings, our boards are designed to
+						handle every flip, grind, and bail. Perfect balance, every time.
+					</div>
+					<div>
+						<ButtonLink color='lime'>Shop Boards</ButtonLink>
+					</div>
+				</div>
+				<div className='grid grid-cols-1 grid-rows-1 place-items-center'>
+					<div className='col-start-1 row-start-1 transition-transform'>
+						<Image
+							src='/paint-background.png'
+							alt=''
+							width={400}
+							height={500}
+						/>
+					</div>
+					<div className='col-start-1 row-start-1 transition-transform'>
+						<Image src='/guy-1.png' alt='' width={330} height={500} />
+					</div>
+				</div>
 			</div>
-			<div className='mb-8' />
-			<ButtonLink color='lime'>Shop Boards</ButtonLink>
 		</Bounded>
 	)
 }
