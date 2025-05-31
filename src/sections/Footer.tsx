7d87aa16ba0ab3f1ae916685856921182666258c
WebRush-Kwork <webrushgithub@gmail.com>
2025-05-31 22:44:28 +0500

    footer without physics

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,20 +1,24 @@
+import { Bounded } from '@/components/Bounded'
+import { FooterPhysics } from '@/components/Footer/FooterPhysics'
+import { Logo } from '@/components/Logo'
 import Nav from '@/components/Nav'
 import Image from 'next/image'
 
 const Footer = () => {
 	return (
 		<footer className='bg-texture bg-zinc-900 text-white overflow-hidden'>
-			<div className='relative h-[75vh]'>
+			<div className='relative h-[75vh] p-10 md:p-14 lg:p-16'>
 				<Image
 					src='/pexels-artempodrez-4816744.jpg'
 					alt=''
 					fill
 					className='object-cover'
 				/>
+				<Logo className='h-20 pointer-events-none mix-blend-exclusion md:h-28' />
 			</div>
-			<div className='px-6 py-10 md:py-12 lg:py-16'>
+			<Bounded as='div'>
 				<Nav />
-			</div>
+			</Bounded>
 		</footer>
 	)
 }
