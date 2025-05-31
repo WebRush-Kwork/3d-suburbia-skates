2662cbe00bc5669a057fbf90b79756d2bb5eadc2
WebRush-Kwork <webrushgithub@gmail.com>
2025-05-31 16:32:51 +0500

    video section is ready

--- src/components/Video/LazyYoutubePlayer.tsx
+++ src/components/Video/LazyYoutubePlayer.tsx
@@ -1,15 +1,41 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+
 type TLazyYouTubePlayer = {
 	youtubeId: string
 }
 
 export function LazyYouTubePlayer({ youtubeId }: TLazyYouTubePlayer) {
+	const containerRef = useRef<HTMLDivElement>(null)
+	const [isInView, setIsInView] = useState<boolean>(false)
+
+	useEffect(() => {
+		const currentContainerRef = containerRef.current
+
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry.isIntersecting) setIsInView(true)
+			},
+			{ rootMargin: '1500px' }
+		)
+
+		if (currentContainerRef) observer.observe(currentContainerRef)
+
+		return () => {
+			currentContainerRef && observer.unobserve(currentContainerRef)
+		}
+	}, [])
+
 	return (
-		<div className='relative h-full w-full'>
-			<iframe
-				src={`https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=1&mute=1&loop=1&playlist=${youtubeId}`}
-				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-				className='pointer-events-none h-full w-full border-0'
-			/>
+		<div className='relative h-full w-full' ref={containerRef}>
+			{isInView && (
+				<iframe
+					src={`https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=1&mute=1&loop=1&playlist=${youtubeId}`}
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+					className='pointer-events-none h-full w-full border-0'
+				/>
+			)}
 		</div>
 	)
 }
--- src/sections/Video.tsx
+++ src/sections/Video.tsx
@@ -1,12 +1,23 @@
 import { Bounded } from '@/components/Bounded'
 import { LazyYouTubePlayer } from '@/components/Video/LazyYoutubePlayer'
+import clsx from 'clsx'
+import Image from 'next/image'
 
 const Video = () => {
 	return (
 		<Bounded className='bg-zinc-900 bg-texture'>
 			<h2 className='sr-only'>Video Reel</h2>
 			<div className='relative aspect-video'>
-				<LazyYouTubePlayer youtubeId='44I29krtxaw' />
+				<div className='video-mask absolute inset-0 bg-brand-lime' />
+				<div className='video-mask relative h-full'>
+					<LazyYouTubePlayer youtubeId='44I29krtxaw' />
+					<Image
+						src='/image-texture.png'
+						alt=''
+						fill
+						className='opacity-50 object-cover pointer-events-none'
+					/>
+				</div>
 			</div>
 		</Bounded>
 	)
