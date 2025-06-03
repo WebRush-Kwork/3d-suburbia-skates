f98a24471c7637bc0ef53be9464936611e4af930
WebRush-Kwork <webrushgithub@gmail.com>
2025-06-04 01:02:09 +0500

    animation ready

--- src/components/Hero/SkateboardScene.tsx
+++ src/components/Hero/SkateboardScene.tsx
@@ -7,6 +7,7 @@ import gsap from 'gsap'
 
 const SkateboardScene = () => {
 	const groupRef = useRef<Group>(null)
+	const originRef = useRef<Group>(null)
 
 	const jumpBoardAnimation = (board: Group) => {
 		gsap
@@ -61,16 +62,37 @@ const SkateboardScene = () => {
 			.to(board.rotation, { x: 0, duration: 0.12, ease: 'none' })
 	}
 
+	const frontSide360 = (origin: Group, board: Group) => {
+		jumpBoardAnimation(board)
+
+		gsap
+			.timeline()
+			.to(board.rotation, {
+				x: -0.6,
+				duration: 0.26,
+				ease: 'none'
+			})
+			.to(board.rotation, { x: 0.4, duration: 0.82, ease: 'power2.in' })
+			.to(
+				origin.rotation,
+				{ y: `+=${Math.PI * 2}`, duration: 0.77, ease: 'none' },
+				0.3
+			)
+			.to(board.rotation, { x: 0, duration: 0.14, ease: 'none' })
+	}
+
 	const onClick = (event: ThreeEvent<MouseEvent>) => {
 		event.stopPropagation()
 
 		const board = groupRef.current
-		if (!board) return
+		const origin = originRef.current
+		if (!board || !origin) return
 
 		const { name } = event.object
 
 		switch (name) {
 			case 'front':
+				frontSide360(origin, board)
 				break
 			case 'middle':
 				kickflip(board)
@@ -85,24 +107,26 @@ const SkateboardScene = () => {
 		<group>
 			<OrbitControls />
 			<Environment files='/hdr/warehouse-256.hdr' />
-			<group ref={groupRef} position={[-0.25, 0, -0.635]}>
-				<group position={[0, -0.086, 0.635]}>
-					<SkateboardModel
-						wheelTextureUrl='/wheel-yellow.png'
-						deckTextureUrl='/yellow-and-black.png'
-					/>
-					<mesh position={[0, 0.27, 0.9]} name='front' onClick={onClick}>
-						<boxGeometry args={[0.6, 0.2, 0.58]} />
-						<meshStandardMaterial visible={false} />
-					</mesh>
-					<mesh position={[0, 0.27, 0]} name='middle' onClick={onClick}>
-						<boxGeometry args={[0.6, 0.1, 1.2]} />
-						<meshStandardMaterial visible={false} />
-					</mesh>
-					<mesh position={[0, 0.27, -0.9]} name='back' onClick={onClick}>
-						<boxGeometry args={[0.6, 0.2, 0.58]} />
-						<meshStandardMaterial visible={false} />
-					</mesh>
+			<group ref={originRef}>
+				<group ref={groupRef} position={[-0.25, 0, -0.635]}>
+					<group position={[0, -0.086, 0.635]}>
+						<SkateboardModel
+							wheelTextureUrl='/wheel-yellow.png'
+							deckTextureUrl='/yellow-and-black.png'
+						/>
+						<mesh position={[0, 0.27, 0.9]} name='front' onClick={onClick}>
+							<boxGeometry args={[0.6, 0.2, 0.58]} />
+							<meshStandardMaterial visible={false} />
+						</mesh>
+						<mesh position={[0, 0.27, 0]} name='middle' onClick={onClick}>
+							<boxGeometry args={[0.6, 0.1, 1.2]} />
+							<meshStandardMaterial visible={false} />
+						</mesh>
+						<mesh position={[0, 0.27, -0.9]} name='back' onClick={onClick}>
+							<boxGeometry args={[0.6, 0.2, 0.58]} />
+							<meshStandardMaterial visible={false} />
+						</mesh>
+					</group>
 				</group>
 			</group>
 			<ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
